ee76699c985e47b6eb498cc6c8594dd22440d8da
marchoag <marc@marchoag.com>
2022-12-19 21:01:45 +0200

    trying to integrate firebase user/pw creation

--- todo.js
+++ todo.js
@@ -1,3 +1,22 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6QlTOioLHGcmT9PScbal-ovLH7_cxErk",
+  authDomain: "doneandmoredone.firebaseapp.com",
+  projectId: "doneandmoredone",
+  storageBucket: "doneandmoredone.appspot.com",
+  messagingSenderId: "534343077398",
+  appId: "1:534343077398:web:426c86c3f53703235fef37"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const form = document.querySelector('#signup-form');
+
+function addEventListeners() {
+  const form = document.querySelector('#signup-form');
+
+  form.addEventListener('submit', event => {
+});
+
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
@@ -10,6 +29,20 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
+  li.setAttribute('draggable', 'true');
+  li.addEventListener('dragstart', function(event) {
+  draggedTask = event.target;
+});
+li.addEventListener('dragover', function(event) {
+  event.preventDefault();
+});
+li.addEventListener('drop', function(event) {
+  event.preventDefault();
+  this.parentNode.insertBefore(draggedTask, this);
+});
+  var hamburger = document.createElement("div");
+hamburger.className = "hamburger";
+li.appendChild(hamburger);
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -33,4 +66,19 @@ function newElement() {
       console.log(ev.target.classList);
     }
   }, false);
+}
+
+document.getElementById("myInput").addEventListener("keydown", function(event) {
+  if (event.code === "Enter") {
+    newElement();
+  }
+});
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function(user) {
+    console.log('Successfully signed up with email and password');
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
 }
